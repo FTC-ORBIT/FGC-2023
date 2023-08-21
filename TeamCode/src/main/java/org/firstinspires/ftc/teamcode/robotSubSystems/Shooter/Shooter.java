cb4941c1c0c9c1469b4ce9a5785ee907ed46384a
Iyar <iyar221c@gmail.com>
2023-08-21 16:04:57 +0300

    made the changes requested for shooter. apriltags check needs to be 'added'. driverButton override for shooting needs to be 'added'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/Shooter/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/Shooter/Shooter.java
@@ -3,10 +3,16 @@ package org.firstinspires.ftc.teamcode.robotSubSystems.Shooter;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class Shooter {
     private static DcMotor shooterMotor1;
     private static DcMotor shooterMotor2;
+    private static VoltageSensor vs;
+    private boolean fault = false;
+    private ElapsedTime faultTime = new ElapsedTime();
+    private boolean readyToShoot = false;
 
     public Shooter(HardwareMap hardwareMap){
         shooterMotor1 = hardwareMap.get(DcMotor.class, "shooterMotor1");
@@ -16,14 +22,14 @@ public class Shooter {
         shooterMotor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         shooterMotor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
-
+        vs = hardwareMap.voltageSensor.get("Motor Controller 1");
     }
 
     public void operate(ShooterState state){
         double power = 0;
         switch (state){
             case SHOOT:
-                power = ShooterConstants.shooterPower;
+                power = ShooterConstants.shooterPower * (12 / vs.getVoltage());
                 break;
             case STOP:
                 power = 0;
@@ -32,7 +38,28 @@ public class Shooter {
         shooterMotor1.setPower(power);
         shooterMotor2.setPower(power);
 
+        update();
     }
 
+    private void update(){
+        if(vs.getVoltage() <= ShooterConstants.faultLimit){
+            fault = true;
+            faultTime.reset();
+        } else {
+            fault = false;
+        }
+
+        if(!getFault() && (float) faultTime.milliseconds() >= ShooterConstants.faultMinTime && AprilTags.inplace || driverButtonPressed) {
+            readyToShoot = true;
+        } else readyToShoot = false;
+    }
+
+    public boolean getFault() {
+        return fault;
+    }
+
+    public boolean isReadyToShoot(){
+        return readyToShoot;
+    }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/Shooter/ShooterConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/Shooter/ShooterConstants.java
@@ -5,5 +5,7 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class ShooterConstants {
     public static float shooterPower = 0.7f;
+    public static float faultLimit = 11f;
+    public static float faultMinTime = 500f;
 
 }
