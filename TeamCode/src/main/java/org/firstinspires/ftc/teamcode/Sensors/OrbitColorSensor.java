d369d78dae6c6f0c47548b1a586c0939697b73ff
dorinon <89009999+dorinon@users.noreply.github.com>
2023-09-06 17:36:44 +0300

    fixed gradle, and removed unnecessary function in OrbitColorSensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensors/OrbitColorSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensors/OrbitColorSensor.java
@@ -15,23 +15,6 @@ public class OrbitColorSensor {
     }
 
 
-    public boolean hasGamePiece() {
-            int colorControlRedCone = 0;
-            int colorControlBlueCone = 0;
-            float[] currentColors = {colorSensor.red(), colorSensor.green(), colorSensor.blue()};
-            for (int i = 0; i < 3; i++){
-                if ((currentColors[i] < Constants.blueCone[i] + Constants.colorRange) && (currentColors[i] > Constants.blueCone[i] - Constants.colorRange)){
-                    colorControlBlueCone ++;
-                }
-            }
-
-            for (int i = 0; i < 3; i++){
-                if ((currentColors[i] < Constants.redCone[i] + Constants.colorRange) && (currentColors[i] > Constants.redCone[i] - Constants.colorRange)){
-                    colorControlRedCone++;
-                }
-            }
-            return (colorControlBlueCone ==3 || colorControlRedCone == 3);
-    }
 
     public void printRGB (Telemetry telemetry){
         telemetry.addData("red", colorSensor.red());
