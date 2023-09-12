d4fe2f5f362970f8e98b590bf521956d7a125937
bar2005 <nc14102005b@big.atid.org.il>
2023-09-12 10:52:53 +0300

    started working on integration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/SubSystemManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/SubSystemManager.java
@@ -1,15 +1,29 @@
 package org.firstinspires.ftc.teamcode.robotSubSystems;
 
+import com.qualcomm.robotcore.hardware.Gamepad;
+
 import org.firstinspires.ftc.teamcode.robotData.GlobalData;
+import org.firstinspires.ftc.teamcode.robotSubSystems.Conveyor.ConveyorState;
+import org.firstinspires.ftc.teamcode.robotSubSystems.Elevator.Elevator;
+import org.firstinspires.ftc.teamcode.robotSubSystems.Elevator.ElevatorState;
+import org.firstinspires.ftc.teamcode.robotSubSystems.intake.IntakeState;
 
 public class SubSystemManager {
 
-    public static RobotState getStateFromJoystick(final boolean[] buttons) {
-        return RobotState.TRAVEL;
+    ConveyorState conveyorState = ConveyorState.STOP;
+    ElevatorState elevatorState = ElevatorState.CLOSED;
+    IntakeState intakeState = IntakeState.STOP;
+
+
+    public static RobotState getStateFromJoystick(Gamepad gamepad) {
+        return gamepad.b ? RobotState.TRAVEL
+                : gamepad.a ? RobotState.INTAKE
+                : gamepad.x ? RobotState.SHOOT_BLUE
+                : gamepad.y ? RobotState.SHOOT_GREEN : null;
     }
 
     private static RobotState getStateFromWantedAndCurrent(final boolean[] buttons,
-                                                           final RobotState robotStateFromJoystick) {
+                                                           final RobotState robotStateFromJoystick) {     // what this function is supposed to do?
         RobotState state_W = robotStateFromJoystick;
         switch (GlobalData.robotState) {
             case INTAKE:
@@ -36,8 +50,6 @@ public class SubSystemManager {
                 break;
         }
 
-        // // Set the state of the LED for telemetry.
-        // LED.setState();
 
         prevRobotState = robotState;
     }
